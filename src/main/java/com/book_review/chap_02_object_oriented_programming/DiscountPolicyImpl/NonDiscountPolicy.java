867fd0144c8f9d7c556aa1ca9da79d4f7d47f470
JIHYEON <jhkim@aptner.com>
2023-01-05 01:57:45 +0900

    hotfix: hotfix code error

--- src/main/java/com/book_review/chap_02_object_oriented_programming/DiscountPolicyImpl/NonDiscountPolicy.java
+++ src/main/java/com/book_review/chap_02_object_oriented_programming/DiscountPolicyImpl/NonDiscountPolicy.java
@@ -4,9 +4,10 @@ import com.book_review.chap_02_object_oriented_programming.DiscountPolicy;
 import com.book_review.chap_02_object_oriented_programming.Money;
 import com.book_review.chap_02_object_oriented_programming.Screening;
 
-public class NonDiscountPolicy extends DiscountPolicy {
+public class NonDiscountPolicy implements DiscountPolicy {
+
     @Override
-    protected Money getDiscountAmount(Screening screening) {
+    public Money calculateDiscountAmount(Screening screening) {
         return Money.ZERO;
     }
 }
