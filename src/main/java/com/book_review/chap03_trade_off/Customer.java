4d6c2f8b2d3d883c4b13dfec5bb189312df67153
JIHYEON <jhkim@aptner.com>
2023-01-08 23:38:28 +0900

    feature: movie reservation application design by data-driven design

---
+++ src/main/java/com/book_review/chap03_trade_off/Customer.java
@@ -0,0 +1,11 @@
+package com.book_review.chap03_trade_off;
+
+public class Customer {
+    private String name;
+    private String id;
+
+    public Customer(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/book_review/chap03_trade_off/DiscountConditionType.java
@@ -0,0 +1,6 @@
+package com.book_review.chap03_trade_off;
+
+public enum DiscountConditionType {
+    SEQUENCE,   // 순번 조건
+    PERIOD      // 기간 조건
+}
