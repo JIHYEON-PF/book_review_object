5c0e905e154a1c393e2787830516bf07f7add7fb
JIHYEON-PF <108737977+JIHYEON-PF@users.noreply.github.com>
2023-01-11 00:27:49 +0900

    feature: Materialization movie reservation application by responsibility allocation (#11)
    Co-authored-by: JIHYEON <jhkim@aptner.com>

---
+++ src/main/java/com/book_review/chap_05_responsibility_allocation/DiscountConditionType.java
@@ -0,0 +1,7 @@
+package com.book_review.chap_05_responsibility_allocation;
+
+public enum DiscountConditionType {
+    SEQUENCE,   // 순번 조건
+    PERIOD      // 기간 조건
+}
+
---
+++ src/main/java/com/book_review/chap_05_responsibility_allocation/Reservation.java
@@ -0,0 +1,7 @@
+package com.book_review.chap_05_responsibility_allocation;
+
+public class Reservation {
+    public Reservation(Customer customer, Screening screening, Money calculateFee, int audienceCount) {
+
+    }
+}
