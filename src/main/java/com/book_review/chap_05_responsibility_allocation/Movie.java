5b9587953f63388b514e629a268f89928d552465
JIHYEON-PF <108737977+JIHYEON-PF@users.noreply.github.com>
2023-01-18 22:31:35 +0900

    refactor: refactoring movie application with GRASP patterns (#12)
    Co-authored-by: JIHYEON <jhkim@aptner.com>

--- src/main/java/com/book_review/chap_05_responsibility_allocation/Movie.java
+++ src/main/java/com/book_review/chap_05_responsibility_allocation/Movie.java
@@ -1,18 +1,22 @@
 package com.book_review.chap_05_responsibility_allocation;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 
-public class Movie {
+public abstract class Movie {
 
     private String title;
     private Duration runningTime;
     private Money fee;
     private List<DiscountCondition> discountConditions;
 
-    private MovieType movieType;
-    private Money discountAmount;
-    private double discountPercent;
+    public Movie(String title, Duration runningTime, Money fee, DiscountCondition ... discountConditions) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountConditions = Arrays.asList(discountConditions);
+    }
 
     public Money calculateMovieFee(Screening screening) {
         if (isDiscountable(screening)) {
@@ -27,28 +31,9 @@ public class Movie {
                 .anyMatch(condition -> condition.isSatisfiedBy(screening));
     }
 
-    private Money calculateDiscountAmount() {
-        switch (movieType) {
-            case AMOUNT_DISCOUNT:
-                return calculateAmountDiscountAmount();
-            case PERCENT_DISCOUNT:
-                return calculatePercentDiscountAmount();
-            case NONE_DISCOUNT:
-                return calculateNoneDiscountAmount();
-        }
-
-        throw new IllegalArgumentException();
+    protected Money getFee() {
+        return fee;
     }
 
-    private Money calculateAmountDiscountAmount() {
-        return discountAmount;
-    }
-
-    private Money calculatePercentDiscountAmount() {
-        return fee.times(discountPercent);
-    }
-
-    private Money calculateNoneDiscountAmount() {
-        return Money.ZERO;
-    }
+    abstract protected Money calculateDiscountAmount();
 }
