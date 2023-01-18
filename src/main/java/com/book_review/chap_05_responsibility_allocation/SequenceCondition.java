ed3dd3ff496de109821c7dcbb4faa14d63611cbe
JIHYEON <jhkim@aptner.com>
2023-01-18 22:21:30 +0900

    refactor: refactoring movie application with GRASP patterns

---
+++ src/main/java/com/book_review/chap_05_responsibility_allocation/SequenceCondition.java
@@ -0,0 +1,13 @@
+package com.book_review.chap_05_responsibility_allocation;
+
+public class SequenceCondition implements DiscountCondition {
+    private int sequence;
+
+    public SequenceCondition(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public boolean isSatisfiedBy(Screening screening) {
+        return sequence == screening.getSequence();
+    }
+}
